2a2ec1285681e60e345a5c8b07ce231ecf781b08
shahiselvam <50788826+shahiselvam@users.noreply.github.com>
2021-08-18 16:27:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(function(req, res, next) {
 app.use(cors({
     origin: [
     
-    'http://localhost:3000'
+    'https://mystifying-lichterman-73d159.netlify.app'
     
   ],
   credentials: true}));
