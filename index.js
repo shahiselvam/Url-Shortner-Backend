2f2cc203da0297d5cccfe25f44b965279ba7e24d
shahiselvam <50788826+shahiselvam@users.noreply.github.com>
2021-08-19 11:50:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,8 +17,20 @@ async function loadApp(){
 
 mongo.connect();
 app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'https://mystifying-lichterman-73d159.netlify.app');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
     res.header('Content-Type', 'application/json;charset=UTF-8')
-    res.header('Access-Control-Allow-Origin', 'https://mystifying-lichterman-73d159.netlify.app' )
+    res.header('Access-Control-Allow-Origin', '*' )
     res.header('Access-Control-Allow-Credentials', true)
     res.header(
       'Access-Control-Allow-Headers',
