c731cc3921451e607acd39e4d507b34f5cb55387
shahiselvam <50788826+shahiselvam@users.noreply.github.com>
2021-08-19 12:26:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -50,8 +50,22 @@ app.use('/' , auth_user);
 app.use('/' , count);
 
 app.use( (req,res,next) => {
+ // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://mystifying-lichterman-73d159.netlify.app');
 
-const token = req.cookies.access_token;
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+const token = req.Cookies.access_token;
 
 if (!token) {
     res.redirect('https://mystifying-lichterman-73d159.netlify.app/login')
