030de71d58779c7611eef591a3d24d29745cad04
shahiselvam <50788826+shahiselvam@users.noreply.github.com>
2021-08-19 12:41:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -35,12 +35,12 @@ mongo.connect();
     next();
 });
 app.use(cors({
-    origin: [
+    "origin": 
     
-    'https://mystifying-lichterman-73d159.netlify.app'
+    "https://mystifying-lichterman-73d159.netlify.app"
     
-  ],
-  Credentials: true,
+  ,
+  "Credentials": true,
   "methods": "GET,HEAD,PUT,PATCH,POST,DELETE"}));
        
 app.use(express.json());
@@ -49,19 +49,7 @@ app.use(cookieParser());
 app.use('/' , auth_user);
 app.use('/' , count);
 
-app.use( function (req,res,next) => {
- res.setHeader('Access-Control-Allow-Origin', 'https://mystifying-lichterman-73d159.netlify.app');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    
+app.use(  (req,res,next) => {    
 const token = req.Cookies.access_token;
 
 if (!token) {
