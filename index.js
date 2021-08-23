5faa8d8fae1febf25c33a3d42bee15e988431cd9
shahiselvam <50788826+shahiselvam@users.noreply.github.com>
2021-08-23 16:44:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.use('/' , count);
 app.use(cookieParser());
 app.use( (req,res,next) => {
 
-const token = req.cookie.access_token;
+const token = req.Cookies.access_token;
 console.log(token);
 
 if (!token) {
